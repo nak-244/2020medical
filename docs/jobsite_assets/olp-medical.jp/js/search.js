063437cf0c7ef54921e41304cf700dff142d137d
nak-244 <t09024656860@gmail.com>
2020-05-13 13:48:02 +0900

    up

--- docs/jobsite_assets/olp-medical.jp/js/search.js
+++ docs/jobsite_assets/olp-medical.jp/js/search.js
@@ -1,7 +1,9 @@
 $(function() {
                   var href = "https://olp-medical.jp/index.cfm";
-                  $('#srh_ken_param,.srh_city_param,.srh_jobtype_child_param,.srh_shift_param,.srh_room_type_param,.sfw').on('change', function() {
-                    var srh_ken_param = $('#srh_ken_param').val();
+                  $('.srh_ken_param,.srh_city_param,.srh_jobtype_child_param,.srh_shift_param,.srh_room_type_param,.sfw').on('change', function() {
+                    var srh_ken_param = $('.srh_ken_param:checked').map(function() {
+                      return $(this).val();
+                    }).get();
                     var srh_city_param = $('.srh_city_param:checked').map(function() {
                       return $(this).val();
                     }).get();
