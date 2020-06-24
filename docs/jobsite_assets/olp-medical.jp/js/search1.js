10b8ded94ac70fffdd7f52f83ba902918d57a78d
nak-244 <t09024656860@gmail.com>
2020-06-24 15:12:12 +0900

    Update search1.js

--- docs/jobsite_assets/olp-medical.jp/js/search1.js
+++ docs/jobsite_assets/olp-medical.jp/js/search1.js
@@ -4,7 +4,7 @@ $(function() {
     var srh_ken_param = $('.srh_ken_param').map(function() {
       return $(this).val();
     }).get();
-    var srh_city_param = $('#srh_city_param:checked').map(function() {
+    var srh_city_param = $('#srh_city_param').map(function() {
       return $(this).val();
     }).get();
     var srh_jobtype_child_param = $('.srh_jobtype_child_param:checked').map(function() {
