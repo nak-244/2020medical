95f6cc1d370fe6f6a063cece0450823cb632fe3a
nak-244 <t09024656860@gmail.com>
2020-06-25 14:48:33 +0900

    Update search1.js

--- docs/jobsite_assets/olp-medical.jp/js/search1.js
+++ docs/jobsite_assets/olp-medical.jp/js/search1.js
@@ -1,33 +1,55 @@
-$(function() {
-  var href = "https://olp-medical.jp/index.cfm";
-  $('#srh_ken_param,#srh_city_param,.srh_ken_param,.srh_city_param,.srh_jobtype_child_param,.srh_shift_param,.srh_room_type_param,.sfw,.sfw1').on('change', function() {
-    var srh_ken_param = $('#srh_ken_param').map(function() {
       return $(this).val();
-    }).get();
-    var srh_city_param = $('#srh_city_param').map(function() {
-      return $(this).val();
-    }).get();
-    var srh_ken_param1 = $('.srh_ken_param').map(function() {
-      return $(this).val();
-    }).get();
-    var srh_city_param1 = $('.srh_city_param').map(function() {
-      return $(this).val();
-    }).get();
-    var srh_jobtype_child_param = $('.srh_jobtype_child_param:checked').map(function() {
-      return $(this).val();
-    }).get();
-    var srh_shift_param = $('.srh_shift_param:checked').map(function() {
-      return $(this).val();
-    }).get();
-    var srh_room_type_param = $('.srh_room_type_param:checked').map(function() {
-      return $(this).val();
-    }).get();
-    var sfw = $('.sfw:checked').map(function() {
-      return $(this).val();
-    }).get();
-    var sfw1 = $('.sfw1').map(function() {
-      return $(this).val();
-    }).get();
+// $(function() {
+//   var href = "https://olp-medical.jp/index.cfm";
+//   $('#srh_ken_param,#srh_city_param,.srh_ken_param,.srh_city_param,.srh_jobtype_child_param,.srh_shift_param,.srh_room_type_param,.sfw,.sfw1').on('change', function() {
+//     var srh_ken_param = $('#srh_ken_param').map(function() {
+//       return $(this).val();
+//     }).get();
+//     var srh_city_param = $('#srh_city_param').map(function() {
+//       return $(this).val();
+//     }).get();
+//     var srh_ken_param1 = $('.srh_ken_param').map(function() {
+//       return $(this).val();
+//     }).get();
+//     var srh_city_param1 = $('.srh_city_param').map(function() {
+//       return $(this).val();
+//     }).get();
+//     var srh_jobtype_child_param = $('.srh_jobtype_child_param:checked').map(function() {
+//       return $(this).val();
+//     }).get();
+//     var srh_shift_param = $('.srh_shift_param:checked').map(function() {
+//       return $(this).val();
+//     }).get();
+//     var srh_room_type_param = $('.srh_room_type_param:checked').map(function() {
+//       return $(this).val();
+//     }).get();
+//     var sfw = $('.sfw:checked').map(function() {
+//       return $(this).val();
+//     }).get();
+//     var sfw1 = $('.sfw1').map(function() {
+//       return $(this).val();
+//     }).get();
+
+    $(function() {
+      var href = "https://olp-medical.jp/index.cfm";
+      $('#srh_ken_param,#srh_city_param,.srh_ken_param,.srh_city_param,.srh_jobtype_child_param,.srh_shift_param,.srh_room_type_param,.sfw,.sfw1').on('change', function() {
+        var srh_ken_param = $('#srh_ken_param').val();
+        var srh_city_param = $('#srh_city_param').val();
+        var srh_ken_param1 = $('.srh_ken_param').val();
+        var srh_city_param1 = $('.srh_city_param').val();
+        var srh_jobtype_child_param = $('.srh_jobtype_child_param:checked').map(function() {
+          return $(this).val();
+        }).get();
+        var srh_shift_param = $('.srh_shift_param:checked').map(function() {
+          return $(this).val();
+        }).get();
+        var srh_room_type_param = $('.srh_room_type_param:checked').map(function() {
+          return $(this).val();
+        }).get();
+        var sfw = $('.sfw:checked').map(function() {
+          return $(this).val();
+        }).get();
+        var sfw1 = $('.sfw1').val();
 
     $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "," + srh_ken_param1 + "&srh_city_param=" + srh_city_param + "," + srh_city_param1 + "&srh_jobtype_child_param=" + srh_jobtype_child_param + "&srh_shift_param=" + srh_shift_param + "&srh_room_type_param=" + srh_room_type_param + "&sfw=" + sfw + sfw1);
   });
