61dd0db4dc6ba7ac0457fffc5d3363391a0d486f
nak-244 <t09024656860@gmail.com>
2020-06-25 17:45:55 +0900

    Update search2.js

--- docs/jobsite_assets/olp-medical.jp/js/search2.js
+++ docs/jobsite_assets/olp-medical.jp/js/search2.js
@@ -1,11 +1,12 @@
 $(function() {
   var href = "https://olp-medical.jp/index.cfm";
   $('#srh_ken_param,.srh_ken_param,#srh_city_param,.srh_city_param,.srh_jobtype_child_param,.srh_shift_param,.srh_room_type_param,.sfw,.sfw1').on('change', function() {
-    var srh_ken_param = $('#srh_ken_param:selected,.srh_ken_param:selected').map(function() {
+    var srh_ken_param = $('#srh_ken_param,.srh_ken_param').map(function() {
       return $(this).val();
     }).get();
-    var srh_city_param = $('#srh_city_param').val();
-    var srh_city_param1 = $('.srh_city_param').val();
+    var srh_city_param = $('#srh_city_param,.srh_city_param').map(function() {
+      return $(this).val();
+    }).get();
     var srh_jobtype_child_param = $('.srh_jobtype_child_param:checked').map(function() {
       return $(this).val();
     }).get();
@@ -20,7 +21,7 @@ $(function() {
     }).get();
 
 
-    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "&srh_city_param=" + srh_city_param + "&srh_city_param=" + srh_city_param1 + "&srh_jobtype_child_param=" + srh_jobtype_child_param + "&srh_shift_param=" + srh_shift_param + "&srh_room_type_param=" + srh_room_type_param + "&sfw=" + sfw );
+    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "&srh_city_param=" + srh_city_param + "&srh_jobtype_child_param=" + srh_jobtype_child_param + "&srh_shift_param=" + srh_shift_param + "&srh_room_type_param=" + srh_room_type_param + "&sfw=" + sfw );
   });
 });
 
