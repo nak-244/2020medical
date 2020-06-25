6acbd93a288cc512c995dbcb416bddc13595b542
nak-244 <t09024656860@gmail.com>
2020-06-25 16:21:16 +0900

    Update search2.js

--- docs/jobsite_assets/olp-medical.jp/js/search2.js
+++ docs/jobsite_assets/olp-medical.jp/js/search2.js
@@ -1,8 +1,8 @@
 $(function() {
   var href = "https://olp-medical.jp/index.cfm";
   $('#srh_ken_param,#srh_city_param,.srh_ken_param,.srh_city_param,.srh_jobtype_child_param,.srh_shift_param,.srh_room_type_param,.sfw,.sfw1').on('change', function() {
-    var srh_ken_param1 = $('#srh_ken_param').val();
-    var srh_ken_param2 = $('.srh_ken_param').val();
+    var srh_ken_param1 = $('#srh_ken_param');
+    var srh_ken_param2 = $('.srh_ken_param');
     var kenparam = [srh_ken_param1,srh_ken_param2];
     var ken_param = kenparam.map(function() {
       return $(this).val();
