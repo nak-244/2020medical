e86f12c8e200b28d0e4eb2f8e4c387dc4bf3b31d
nak-244 <t09024656860@gmail.com>
2020-06-25 15:34:23 +0900

    Update search1.js

--- docs/jobsite_assets/olp-medical.jp/js/search1.js
+++ docs/jobsite_assets/olp-medical.jp/js/search1.js
@@ -19,7 +19,7 @@ $(function() {
     }).get();
     var sfw1 = $('.sfw1').val();
 
-    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "　" + srh_ken_param1 + "&srh_city_param=" + srh_city_param + "　" + srh_city_param1 + "&srh_jobtype_child_param=" + srh_jobtype_child_param + "&srh_shift_param=" + srh_shift_param + "&srh_room_type_param=" + srh_room_type_param + "&sfw=" + sfw + sfw1);
+    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "," + srh_ken_param1 + "&srh_city_param=" + srh_city_param + "," + srh_city_param1 + "&srh_jobtype_child_param=" + srh_jobtype_child_param + "&srh_shift_param=" + srh_shift_param + "&srh_room_type_param=" + srh_room_type_param + "&sfw=" + sfw + "," + sfw1);
   });
 });
 
