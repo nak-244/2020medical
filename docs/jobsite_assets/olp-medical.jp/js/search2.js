8d497cdbab47143e26c3348deab6e57a97fb0ece
nak-244 <t09024656860@gmail.com>
2020-06-25 17:41:31 +0900

    Update search2.js

--- docs/jobsite_assets/olp-medical.jp/js/search2.js
+++ docs/jobsite_assets/olp-medical.jp/js/search2.js
@@ -1,10 +1,9 @@
 $(function() {
   var href = "https://olp-medical.jp/index.cfm";
   $('#srh_ken_param,.srh_ken_param,#srh_city_param,.srh_city_param,.srh_jobtype_child_param,.srh_shift_param,.srh_room_type_param,.sfw,.sfw1').on('change', function() {
-    var srh_ken_param = $('#srh_ken_param').map(function() {
+    var srh_ken_param = $('#srh_ken_param,.srh_ken_param').map(function() {
       return $(this).val();
     }).get();
-    var srh_ken_param1 = $('.srh_ken_param').val();
     var srh_city_param = $('#srh_city_param').val();
     var srh_city_param1 = $('.srh_city_param').val();
     var srh_jobtype_child_param = $('.srh_jobtype_child_param:checked').map(function() {
@@ -21,7 +20,7 @@ $(function() {
     }).get();
 
 
-    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "," + srh_ken_param1 + "&srh_city_param=" + srh_city_param + "&srh_city_param=" + srh_city_param1 + "&srh_jobtype_child_param=" + srh_jobtype_child_param + "&srh_shift_param=" + srh_shift_param + "&srh_room_type_param=" + srh_room_type_param + "&sfw=" + sfw );
+    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_ken_param=" + srh_ken_param + "&srh_city_param=" + srh_city_param + "&srh_city_param=" + srh_city_param1 + "&srh_jobtype_child_param=" + srh_jobtype_child_param + "&srh_shift_param=" + srh_shift_param + "&srh_room_type_param=" + srh_room_type_param + "&sfw=" + sfw );
   });
 });
 
