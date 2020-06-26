5dbf47f18fa6bcb6b03c71202573832c7e0ccc3f
nak-244 <t09024656860@gmail.com>
2020-06-26 15:05:08 +0900

    Update search2.js

--- docs/jobsite_assets/olp-medical.jp/js/search2.js
+++ docs/jobsite_assets/olp-medical.jp/js/search2.js
@@ -2652,41 +2652,41 @@ $(document).ready(function() {
 });
 
 //1つめ
-//エリアを絞り込むと、エリアに対応した都道府県のみ検索条件として表示させる
-function ajaxAreaToKen() {
-  var url = "index.cfm?fuseaction=job.ajax_areatopref";
-  var selected_ken = $('select[name="srh_ken_param"]').val();
-  var selected_city = $('select[name="srh_city_param"]').val();
-
-  var arr_area_param = [];
-  $('[name="srh_area_param"]:checked').each(function() {
-    arr_area_param.push($(this).val());
-  });
-  var str_area_param = arr_area_param.join(','); //現在選択中のエリアID(カンマ区切り)
-
-  $.post(url, {
-    aj_areatopref: true,
-    aj_area_id: str_area_param
-  }, function(response) {
-    if (!response || response.substr(0, 7) != '<option') {
-      var resHtml = '<option value="">----</option>'; //デフォルト値
-    } else {
-      var resHtml = response;
-    }
-    $('select[name="srh_ken_param"]').html(resHtml);
-
-    resumeSelectedKen(selected_ken);
-    ajaxKenToCity(true, selected_city);
-  });
-}
-//あらかじめ選択していた都道府県を再度選択状態にする
-function resumeSelectedKen(ken_jis) {
-  $('select[name="srh_ken_param"] option').each(function() {
-    if ($(this).val() == ken_jis) {
-      $('select[name="srh_ken_param"]').val(ken_jis);
-    }
-  });
-}
+// //エリアを絞り込むと、エリアに対応した都道府県のみ検索条件として表示させる
+// function ajaxAreaToKen() {
+//   var url = "index.cfm?fuseaction=job.ajax_areatopref";
+//   var selected_ken = $('select[name="srh_ken_param"]').val();
+//   var selected_city = $('select[name="srh_city_param"]').val();
+//
+//   var arr_area_param = [];
+//   $('[name="srh_area_param"]:checked').each(function() {
+//     arr_area_param.push($(this).val());
+//   });
+//   var str_area_param = arr_area_param.join(','); //現在選択中のエリアID(カンマ区切り)
+//
+//   $.post(url, {
+//     aj_areatopref: true,
+//     aj_area_id: str_area_param
+//   }, function(response) {
+//     if (!response || response.substr(0, 7) != '<option') {
+//       var resHtml = '<option value="">----</option>'; //デフォルト値
+//     } else {
+//       var resHtml = response;
+//     }
+//     $('select[name="srh_ken_param"]').html(resHtml);
+//
+//     resumeSelectedKen(selected_ken);
+//     ajaxKenToCity(true, selected_city);
+//   });
+// }
+// //あらかじめ選択していた都道府県を再度選択状態にする
+// function resumeSelectedKen(ken_jis) {
+//   $('select[name="srh_ken_param"] option').each(function() {
+//     if ($(this).val() == ken_jis) {
+//       $('select[name="srh_ken_param"]').val(ken_jis);
+//     }
+//   });
+// }
 //県を絞り込むと、県に対応した市区郡のみ検索条件として表示させる
 //resume_flg=trueにすると、resume_idをselectedにする(ページ読み込み直後や、エリア変更により都道府県selectが変更された時に使用する)
 function ajaxKenToCity(resume_flg, resume_id) {
